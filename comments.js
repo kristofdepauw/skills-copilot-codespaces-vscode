6d5d32d08c9d1a54d893425690eca4e7504dba73
kristofdepauw <37872919+kristofdepauw@users.noreply.github.com>
2024-02-25 22:02:20 +0000

    third commit

--- comments.js
+++ comments.js
@@ -1,11 +1,4 @@
-// create a web server that listens on port 3000
+// Create web server
 
-// import the express module
-
-const express = require('express');
-
-// create a new web server
-
-const app = express();
 
 
