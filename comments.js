c4f6e6c4bdfa15c268aea2d67af7091e346d6804
kristofdepauw <37872919+kristofdepauw@users.noreply.github.com>
2024-02-25 22:01:06 +0000

    third commit

--- comments.js
+++ comments.js
@@ -1,78 +1,11 @@
-// create web server
+// create a web server that listens on port 3000
 
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var qs = require('querystring');
-var template = require('./lib/template.js');
-var path = require('path');
-var sanitizeHtml = require('sanitize-html');
+// import the express module
 
-var app = http.createServer(function(request, response) {
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    var pathname = url.parse(_url, true).pathname;
+const express = require('express');
 
-    if (pathname === '/') {
-        if (queryData.id === undefined) {
-            fs.readdir('./data', function(error, filelist) {
-                var title = 'Welcome';
-                var description = 'Hello, Node.js';
-                var list = template.list(filelist);
-                var html = template.HTML(
-                    title,
-                    list,
-                    `<h2>${title}</h2>${description}`,
-                    `<a href="/create">create</a>`
-                );
-                response.writeHead(200);
-                response.end(html);
-            });
-        } else {
-            fs.readdir('./data', function(error, filelist) {
-                var filteredId = path.parse(queryData.id).base;
-                fs.readFile(`data/${filteredId}`, 'utf8', function(err, description) {
-                    var title = queryData.id;
-                    var sanitizedTitle = sanitizeHtml(title);
-                    var sanitizedDescription = sanitizeHtml(description, {
-                        allowedTags: ['h1']
-                    });
-                    var list = template.list(filelist);
-                    var html = template.HTML(
-                        sanitizedTitle,
-                        list,
-                        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-                        `<a href="/create">create</a>
-                         <a href="/update?id=${sanitizedTitle}">update</a>
-                         <form action="delete_process" method="post">
-                             <input type="hidden" name="id" value="${sanitizedTitle}">
-                             <input type="submit" value="delete">
-                         </form>`
-                    );
-                    response.writeHead(200);
-                    response.end(html);
-                });
-            });
-        }
-    } else if (pathname === '/create') {
-        fs.readdir('./data', function(error, filelist) {
-            var title = 'WEB - create';
-            var list = template.list(filelist);
-            var html = template.HTML(
-                title,
-                list,
-                `
-                <form action="/create_process" method="post">
-                    <p><input type="text" name="title" placeholder="title"></p>
-                    <p>
-                        <textarea name="description" placeholder="description"></textarea>
-                    </p>
-                    <p>
-                        <input type="submit">
-                    </p>
-                </form>
-                `,
-                ''
-            );
-            response.writeHead(200);
-            response.end(html);
+// create a new web server
+
+const app = express();
+
+
